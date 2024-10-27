c9140b7846f841cce42bdf98170e71476aebc7c9
Alexis Dechenais <alexis.dechenais@etud.univ-jfc.fr>
2024-10-27 15:09:55 +0100

    passage aux boutons sans mutliplexeur (plateau réduit à 12 cases)

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,35 @@
 #include <Arduino.h>
 
+// Définition des pins pour les boutons
+const int buttonPins[12] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+const char* messages[12] = {
+    "Départ", //button 1 pressed
+    "Boulevard de Belleville",//button 2 pressed
+    "Avenue de la Republique",//button 3 pressed
+    "En prison/ Visite simple",//button 4 pressed
+    "Boulevard de la Villette",//button 5 pressed
+    "Place Pigalle",//button 6 pressed
+    "Parc gratuit",//button 7 pressed
+    "Avenue Matignon",//button 8 pressed
+    "Rue la Fayette",//button 9 pressed
+    "Allez en prison",//button 10 pressed
+    "Avenue de Breteuil",//button 11 pressed
+    "Rue de la paix"//button 12 pressed
+};
+
 void setup() {
     Serial.begin(9600);
+    // Configuration des pins en entrée
+    for (int i = 0; i < 12; i++) {
+        pinMode(buttonPins[i], INPUT);
+    }
 }
 
 void loop() {
-    Serial.println("Rue de la paix");
-    delay(3000);  // Affichage toutes les 3 secondes
+    for (int i = 0; i < 12; i++) {
+        if (digitalRead(buttonPins[i]) == HIGH) {
+            Serial.println(messages[i]);  // Affiche le message correspondant
+            delay(500); // Délai pour éviter les rebonds
+        }
+    }
 }
